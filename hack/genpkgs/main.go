4d66cbcae207eeb8000cff52a3a90f26b21a279f
David Juhasz <djjuhasz@gmail.com>
2025-01-03 14:03:50 -0800

    Write generated packages directly to the database

--- hack/genpkgs/main.go
+++ hack/genpkgs/main.go
@@ -1,62 +1,215 @@
 package main
 
 import (
-	"encoding/csv"
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
-	"os"
 	"strconv"
+	"time"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
 )
 
 const (
 	// How many records we want to generate.
-	datasetSize = 1000
+	datasetSize = 105
 
 	// Size of each batch that we write to the CSV.
-	batchSize = 100
+	batchSize = 10
 )
 
-func main() {
-	w := csv.NewWriter(os.Stdout)
+type database interface {
+	connect() error
+	init() error
+	batchInsertPkgs([]pkg) (int, error)
+	insertPkgs([]pkg) (int, error)
+}
 
-	var c int
-	for {
-		c++
-		if err := w.Write(gen(c)); err != nil {
-			log.Println("error writing record to csv:", err)
-		}
-		if c%batchSize == 0 {
-			w.Flush()
-		}
-		if c == datasetSize {
-			break
+type Config struct{}
+
+func main() {
+	db := &mysqlDB{
+		cfg: mysql.Config{
+			User:      "enduro",
+			Passwd:    "enduro123",
+			Net:       "tcp",
+			Addr:      "127.0.0.1:3306",
+			DBName:    "enduro",
+			ParseTime: true,
+		},
+	}
+	c := newCmd(Config{}, db)
+
+	out, err := c.run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(out)
+}
+
+type pkg struct {
+	ID          string
+	Name        string
+	WorkflowID  string
+	RunID       uuid.UUID
+	AIPID       uuid.UUID
+	LocationID  uuid.UUID
+	Status      string
+	CreatedAt   time.Time
+	StartedAt   time.Time
+	CompletedAt time.Time
+}
+
+func (p pkg) toSlice() []any {
+	return []any{
+		p.ID,
+		p.Name,
+		p.WorkflowID,
+		p.RunID,
+		p.AIPID,
+		p.LocationID,
+		p.Status,
+		p.CreatedAt,
+		p.StartedAt,
+		p.CompletedAt,
+	}
+}
+
+type cmd struct {
+	cfg Config
+	db  database
+}
+
+func newCmd(cfg Config, db database) *cmd {
+	return &cmd{cfg: cfg, db: db}
+}
+
+func (c *cmd) run() (string, error) {
+	if err := c.db.connect(); err != nil {
+		return "", fmt.Errorf("DB connect: %v", err)
+	}
+
+	if err := c.db.init(); err != nil {
+		return "", fmt.Errorf("DB init: %v", err)
+	}
+
+	// Generate packages
+	pkgs := make([]pkg, datasetSize)
+	for i := range datasetSize {
+		pkgs[i] = gen(i + 1)
+	}
+
+	count, err := c.db.batchInsertPkgs(pkgs)
+	if err != nil {
+		return "", fmt.Errorf("insert packages: %v", err)
+	}
+
+	return fmt.Sprintf("%d packages inserted!\n", count+1), nil
+}
+
+type mysqlDB struct {
+	cfg  mysql.Config
+	conn *sql.DB
+}
+
+func (d *mysqlDB) connect() error {
+	// Get a database handle.
+	conn, err := sql.Open("mysql", d.cfg.FormatDSN())
+	if err != nil {
+		return err
+	}
+	d.conn = conn
+
+	return nil
+}
+
+func (d *mysqlDB) init() error {
+	if _, err := d.conn.Exec("DELETE FROM package;"); err != nil {
+		return fmt.Errorf("delete packages: %v", err)
+	}
+
+	return nil
+}
+
+func (d *mysqlDB) batchInsertPkgs(pkgs []pkg) (int, error) {
+	var count int
+	batch := make([]pkg, 0, batchSize)
+
+	for _, p := range pkgs {
+		batch = append(batch, p)
+		count++
+
+		if count%batchSize == 0 {
+			if _, err := d.insertPkgs(batch); err != nil {
+				return 0, fmt.Errorf("batch: %v", err)
+			}
+			batch = make([]pkg, 0, batchSize)
 		}
 	}
 
-	w.Flush()
-	if err := w.Error(); err != nil {
-		log.Fatal(err)
+	if len(batch) > 0 {
+		if _, err := d.insertPkgs(batch); err != nil {
+			return 0, fmt.Errorf("batch: %v", err)
+		}
+	}
+
+	return count, nil
+}
+
+func (d *mysqlDB) insertPkgs(pkgs []pkg) (int, error) {
+	args := make([]any, 0, 10*len(pkgs))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	q := "INSERT INTO package VALUES "
+	t := "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?),"
+
+	for _, p := range pkgs {
+		if p.ID == "" {
+			break
+		}
+
+		q += t
+		args = append(args, p.toSlice()...)
+	}
+
+	// Trim final comma.
+	q = q[0 : len(q)-1]
+	q += ";"
+
+	r, err := d.conn.ExecContext(ctx, q, args...)
+	if err != nil {
+		return 0, fmt.Errorf("insert packages: %v", err)
+	}
+
+	count, err := r.RowsAffected()
+	if err != nil {
+		count = int64(len(pkgs))
+	}
+
+	return int(count), nil
+}
+
+func gen(i int) pkg {
+	const doneStatus string = "2"
+	return pkg{
+		ID:          strconv.Itoa(i),
+		Name:        fmt.Sprintf("DPJ-SIP-%s.tar", id()),
+		WorkflowID:  fmt.Sprintf("processing-workflow-%s", id()),
+		RunID:       uuid.New(),
+		AIPID:       uuid.New(),
+		LocationID:  uuid.New(),
+		Status:      doneStatus,
+		CreatedAt:   time.Date(2019, 11, 21, 17, 36, 10, 0, time.UTC),
+		StartedAt:   time.Date(2019, 11, 21, 17, 36, 11, 0, time.UTC),
+		CompletedAt: time.Date(2019, 11, 21, 17, 42, 12, 0, time.UTC),
 	}
 }
 
 func id() string {
 	return uuid.New().String()
-}
-
-func gen(c int) []string {
-	const doneStatus string = "2"
-	return []string{
-		strconv.Itoa(c),
-		fmt.Sprintf("DPJ-SIP-%s.tar", id()),         // name
-		fmt.Sprintf("processing-workflow-%s", id()), // workflow_id
-		id(),                  // run_id
-		id(),                  // aip_id
-		id(),                  // location_id
-		doneStatus,            // status
-		"2019-11-21 17:36:10", // created_at
-		"2019-11-21 17:36:11", // started_at
-		"2019-11-21 17:42:12", // completed_at
-	}
 }
